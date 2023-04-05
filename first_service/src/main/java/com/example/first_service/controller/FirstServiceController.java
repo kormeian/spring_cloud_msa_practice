5e8b752bff34ef699ea7130228d9feedf61cf29f
IAN <ooliskoo@naver.com>
2023-04-06 01:14:24 +0900

    feat : add filter

--- first_service/src/main/java/com/example/first_service/controller/FirstServiceController.java
+++ first_service/src/main/java/com/example/first_service/controller/FirstServiceController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
 
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the First Service";
+	}
+	@GetMapping("/check")
+	public String check() {
+		return "Hi, there. This is a message from First Service";
 	}
 }
--- second_service/src/main/java/com/example/second_service/controller/SecondServiceController.java
+++ second_service/src/main/java/com/example/second_service/controller/SecondServiceController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
 
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the Second Service";
+	}
+
+	@GetMapping("/check")
+	public String check() {
+		return "Hi, there. This is a message from Second Service";
 	}
 }
