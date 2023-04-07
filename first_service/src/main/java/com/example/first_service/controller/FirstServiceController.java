493c851e748b3add592c37969333d46a6ba23070
IAN <ooliskoo@naver.com>
2023-04-07 21:07:17 +0900

    Feat : add load balance test api
    check local server port

--- first_service/src/main/java/com/example/first_service/controller/FirstServiceController.java
+++ first_service/src/main/java/com/example/first_service/controller/FirstServiceController.java
@@ -1,19 +1,35 @@
 package com.example.first_service.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+	Environment env;
+	@Autowired
+	public FirstServiceController(Environment env) {
+		this.env = env;
+	}
+	@Value("local.server.port")
+	private String port;
 
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the First Service";
 	}
 	@GetMapping("/check")
-	public String check() {
-		return "Hi, there. This is a message from First Service";
+	public String check(HttpServletRequest request) {
+		log.info("Server port={}", request.getServerPort());
+
+		return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
 	}
 }
