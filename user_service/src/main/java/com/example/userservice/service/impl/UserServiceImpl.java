6762ceb20f5b7e6cde36ab42523354f2d132adc9
IAN <ooliskoo@naver.com>
2023-04-09 23:55:22 +0900

    feat : add password encoder

--- user_service/src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
+++ user_service/src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,14 +16,15 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
 		userDto.setUserId(UUID.randomUUID().toString());
+		userDto.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
-		userEntity.setEncryptedPwd("test");
 		userRepository.save(userEntity);
 		return modelMapper.map(userEntity, UserDto.class);
 	}
